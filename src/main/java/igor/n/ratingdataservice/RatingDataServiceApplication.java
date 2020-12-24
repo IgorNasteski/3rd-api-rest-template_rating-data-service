510e6d257f5d9d086fa11d1a8158c9a1fa604352
Igor Nasteski <igor.nasteski92@gmail.com>
2020-12-24 02:45:30 +0100

    added class RatingResponse and in it List of Rating class as a property, because i don't want to send as a restTemplate response List of Rating, i want to send Object of RatingResponse which has one field - List of Rating

--- src/main/java/igor/n/ratingdataservice/RatingDataServiceApplication.java
+++ src/main/java/igor/n/ratingdataservice/RatingDataServiceApplication.java
@@ -2,9 +2,16 @@ package igor.n.ratingdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RatingDataServiceApplication {
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatingDataServiceApplication.class, args);
--- src/main/java/igor/n/ratingdataservice/controller/RatingController.java
+++ src/main/java/igor/n/ratingdataservice/controller/RatingController.java
@@ -1,42 +1,35 @@
 package igor.n.ratingdataservice.controller;
 
 import igor.n.ratingdataservice.model.Rating;
+import igor.n.ratingdataservice.model.RatingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/ratings")
 public class RatingController {
 
-    @GetMapping("/{userId}")
-    public Rating getRatings(@PathVariable("userId")String userId){
-        ratingList.add(rating1);
-        ratingList.add(rating2);
-        ratingList.add(rating3);
-        ratingList.add(rating4);
-        ratingList.add(rating5);
+    //@Autowired
+    //private RestTemplate restTemplate;
 
-        /*for(int i=0; i<ratingList.size(); i++){
-            if(ratingList.get(i).getMovieId().equalsIgnoreCase(movieId)){
-                return ratingList.get(i);
-            }
+    @GetMapping("users/{userId}")
+    public RatingResponse getRatings(@PathVariable("userId")String userId) {
+        RatingResponse ratingResponse = new RatingResponse();
 
-        }*/
+        ratingResponse.setRatings(
+                Arrays.asList(
+                        new Rating("100", 3),
+                        new Rating("200", 4)
+                )
+        );
 
-        return ratingList.get(Integer.parseInt(userId)-1);  //isao -1 da bih kad ukucam 1 dobio
-        //return null;                      //LOTR a ne HP, jer je LOTR nulti clan niza a ne prvi
-    }                                       //jer korisnik u requestu ne zna da kad ukuca 1, da meni daje drugi clan niza
-                                            //jer pocinju od 0
-    List<Rating> ratingList = new ArrayList<Rating>();
-    Rating rating1 = new Rating("1", 1);
-    Rating rating2 = new Rating("2", 2);
-    Rating rating3 = new Rating("3", 3);
-    Rating rating4 = new Rating("4", 4);
-    Rating rating5 = new Rating("5", 5);
+        return ratingResponse;
+    }
 
 }
